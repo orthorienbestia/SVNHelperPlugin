8e2cc6b49b54c400dd55a1172b313247fffb8ce3
Akshay <akshay@solovetech.com>
2020-01-04 21:07:21 +0530

    some commands added move command testing remaining

--- SVNHelperPlugin/Assets/SVNPluginHelper/Scripts/SVNCommands.cs
+++ SVNHelperPlugin/Assets/SVNPluginHelper/Scripts/SVNCommands.cs
@@ -4,30 +4,123 @@
  *https://tortoisesvn.net/docs/release/TortoiseSVN_en/tsvn-automation.html#tsvn-automation-basics
  */
 
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics;
 using UnityEditor;
 using UnityEngine;
-using Debug = UnityEngine.Debug;
+
 namespace SVNPluginHelper.Scripts
 {
-    public class SVNCommands
+    public static class SVNCommands
     {
-        [MenuItem("SVNCommands/Commit")]
+        #region Commands For Menu
+
+        [MenuItem("SVN Commands/Commit")]
         private static void CommitFromMenu()
         {
-            var svn_command =  @"commit /path:" + Application.dataPath + " /logmsg:\"test log message\" /closeonend:0";
+            var svn_command = @"commit /path:" + Application.dataPath + " /logmsg:\"dummy log message\" /closeonend:0";
             SVNProcessHandler.ExecuteCommand(svn_command);
         }
 
-        [MenuItem("Assets/SVNCommands/Rename")]
+        [MenuItem("SVN Commands/Update/Update to Head")]
+        private static void UpdateToHeadFromMenu()
+        {
+            var svn_command = @"update /path:" + Application.dataPath + " /closeonend:0";
+            SVNProcessHandler.ExecuteCommand(svn_command);
+        }
+
+        [MenuItem("SVN Commands/Update/Update to Revision")]
+        private static void UpdateToRevisionFromMenu()
+        {
+            var svn_command = @"update /path:" + Application.dataPath + " /rev /closeonend:0";
+            SVNProcessHandler.ExecuteCommand(svn_command);
+        }
+
+        [MenuItem("SVN Commands/Repository Status")]
+        private static void RepoStatusFromMenu()
+        {
+            var svn_command = @"repostatus /path:" + Application.dataPath + " /closeonend:0";
+            SVNProcessHandler.ExecuteCommand(svn_command);
+        }
+
+        [MenuItem("SVN Commands/Repository Browser")]
+        private static void RepoBrowserFromMenu()
+        {
+            var svn_command = @"repobrowser /path:" + Application.dataPath + " /closeonend:0";
+            SVNProcessHandler.ExecuteCommand(svn_command);
+        }
+        
+        [MenuItem("SVN Commands/Resolve")]
+        private static void ResolveFromMenu()
+        {
+            var svn_command = @"resolve /path:" + Application.dataPath + " /closeonend:0";
+            SVNProcessHandler.ExecuteCommand(svn_command);
+        }
+        
+        [MenuItem("SVN Commands/Switch")]
+        private static void SwitchFromMenu()
+        {
+            var svn_command = @"switch /path:" + Application.dataPath + " /closeonend:0";
+            SVNProcessHandler.ExecuteCommand(svn_command);
+        }
+        #endregion
+
+        #region Commands For Project Window
+
+        [MenuItem("Assets/SVN Commands/Commit")]
+        private static void CommitFromAssets()
+        {
+            var selectedObjectName = AssetDatabase.GetAssetPath(Selection.activeObject);
+            var svn_command = @"commit /path:" + selectedObjectName + " /closeonend:0";
+            SVNProcessHandler.ExecuteCommand(svn_command);
+        }
+        
+        [MenuItem("Assets/SVN Commands/Rename")]
         private static void RenameFromAssets()
         {
             var selectedObjectName = AssetDatabase.GetAssetPath(Selection.activeObject);
             var svn_command = @"rename /path:" + selectedObjectName + " /closeonend:0";
             SVNProcessHandler.ExecuteCommand(svn_command);
         }
+
+        [MenuItem("Assets/SVN Commands/Revert")]
+        private static void RevertFromAssets()
+        {
+            var selectedObjectName = AssetDatabase.GetAssetPath(Selection.activeObject);
+            var svn_command = @"revert /path:" + selectedObjectName + " /closeonend:0";
+            SVNProcessHandler.ExecuteCommand(svn_command);
+        }
+
+        [MenuItem("Assets/SVN Commands/Add")]
+        private static void AddFromAssets()
+        {
+            var selectedObjectName = AssetDatabase.GetAssetPath(Selection.activeObject);
+            var svn_command = @"add /path:" + selectedObjectName + " /closeonend:0";
+            SVNProcessHandler.ExecuteCommand(svn_command);
+        }
+        
+        [MenuItem("Assets/SVN Commands/Resolve")]
+        private static void ResolveFromAssets()
+        {
+            var selectedObjectName = AssetDatabase.GetAssetPath(Selection.activeObject);
+            var svn_command = @"resolve /path:" + selectedObjectName + " /closeonend:0";
+            SVNProcessHandler.ExecuteCommand(svn_command);
+        }
+        
+        [MenuItem("Assets/SVN Commands/Diff Checker")]
+        private static void DiffCheckerFromAssets()
+        {
+            var selectedObjectName = AssetDatabase.GetAssetPath(Selection.activeObject);
+            var svn_command = @"diff /path:" + selectedObjectName + " /closeonend:0";
+            SVNProcessHandler.ExecuteCommand(svn_command);
+        }
+        
+        [MenuItem("Assets/SVN Commands/Move")]
+        private static void MoveFromAssets()
+        {
+            var selectedObjectName = AssetDatabase.GetAssetPath(Selection.activeObject);
+            var svn_command = @"/c svn move "+selectedObjectName+" "+Application.dataPath;
+            SVNProcessHandler.ExecuteSVNCommand(svn_command);
+        }
+
+        #endregion
     }
 }
--- SVNHelperPlugin/Assets/SVNPluginHelper/Scripts/SVNProcessHandler.cs
+++ SVNHelperPlugin/Assets/SVNPluginHelper/Scripts/SVNProcessHandler.cs
@@ -1,16 +1,16 @@
 using System;
 using System.Diagnostics;
-using UnityEngine;
 using Debug = UnityEngine.Debug;
 
 namespace SVNPluginHelper.Scripts
 {
-    public class SVNProcessHandler
+    public static class SVNProcessHandler
     {
         private static string commandPrefix = @"/c " + "TortoiseProc.exe /command:";
-        
+
         internal static void ExecuteCommand(string SVNCommand)
         {
+            var cmd_name = SVNCommand.Split(' ')[0];
             SVNCommand = commandPrefix + SVNCommand;
             try
             {
@@ -24,19 +24,52 @@ namespace SVNPluginHelper.Scripts
                 int eCode = process.ExitCode;
                 if (eCode == 0)
                 {
-                    Debug.Log("Couldn't be executed");
+                    Debug.Log($"{cmd_name} Executed Successfully with plugin warnings");
                 }
                 else
                 {
-                    Debug.Log("Executed Successfully");
+                    Debug.Log($"{cmd_name} Executed Successfully");
                 }
             }
             catch (Exception ex)
             {
                 //Catch Exception
-                Debug.Log("Failed to Commit");
-                Debug.Log("Caught Exception: " + ex);
+                Debug.Log($"{cmd_name}: Failed to execute command " + SVNCommand);
+                Debug.Log($"{cmd_name}: Caught Exception: " + ex);
             }
         }
+        
+        
+        internal static void ExecuteSVNCommand(string SVNCommand)  
+        {  
+            try  
+            {  
+                Process.Start("CMD.exe",SVNCommand);
+                // ProcessStartInfo processStartInfo = new ProcessStartInfo();  
+                // //processStartInfo.WorkingDirectory = @"C:\Program Files\TortoiseSVN\bin";  
+                // processStartInfo.RedirectStandardInput = true;  
+                // processStartInfo.CreateNoWindow = true;  
+                // processStartInfo.WindowStyle = ProcessWindowStyle.Normal;  
+                // processStartInfo.FileName = "cmd.exe";                  
+                // processStartInfo.UseShellExecute = false;  
+                // processStartInfo.Arguments = SVNCommand;  
+                // Process process = Process.Start(processStartInfo);  
+                // process.WaitForExit();  
+                //
+                // int eCode = process.ExitCode;  
+                // if (eCode == 0)  
+                // {  
+                //     //Success  
+                // }  
+                // else  
+                // {  
+                //     //Failed
+                // }  
+            }  
+            catch (Exception ex)  
+            {  
+                //Catch Exception  
+            }  
+        }  
     }
 }
